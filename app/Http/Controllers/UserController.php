a3dca9df6d2a99f3d3e441c93f92ae940a08f872
Juan Manuel <jmpascagaza53@ucatolica.edu.co>
2018-06-02 22:49:55 -0500

    Actualizar constraseña

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Input;
 use Illuminate\Http\Request;
 use App\Services\SessionService;
 use DB;
+use Hash;
 use App\Quotation;
 
 class UserController extends Controller
@@ -24,37 +25,73 @@ class UserController extends Controller
 
     public function get()
     {
-
         $usuario = DB::select('select  @rownum:=@rownum+1 AS rownum, 
-                `codigo`,`nombres`,`apellidos`,`celular`,`correo`,`tipo_idTipo`,`estado` from `usuario`');
+        `codigo`,`nombres`,`apellidos`,`celular`,`correo`,`tipo_idTipo`,`estado` from `usuario`');
         return view('usuario-listar')->with('session', $this->session->getSession())->with('usuario', $usuario); 
         
     }
-
+    public function getKind()
+    {
+        $kinds = DB::select("select idTipo as id, nombreTipo as detalle  from tipo order by idTipo");
+        return $kinds;
+    }
+    public function getUser($userid)
+    {
+        $usuario = DB::select('select `codigo`,`nombres`,`apellidos`,`celular`,`correo`,`tipo_idTipo`,`estado`, password from `usuario` where codigo=?', array($userid));
+        return $usuario;
+    }
     public function getDetailUser()
     {
         $data_session = $this->session->getSession();
         $userid = Input::get('userid', 0);
-        $cview = Input::get('view', '');
         if($data_session->status !== 1){
             return redirect('/');
         }elseif(intval($data_session->code) !== intval($userid) && $data_session->rol !== 1){
             return redirect('/');
         }else{            
-            $usuario = DB::select('select 
-                    `codigo`,`nombres`,`apellidos`,`celular`,`correo`,`tipo_idTipo`,`estado` from `usuario` where codigo=?', array($userid));
-            $kinds = DB::select("select idTipo as id, nombreTipo as detalle  from tipo order by idTipo");
+            $usuario = self::getUser($userid);
+            $kinds = self::getKind();
             return view('usuario-actualizar')->with('session', $this->session->getSession())
-            ->with('kinds', $kinds)->with('usuario', $usuario[0])->with('cview', $cview);
+            ->with('kinds', $kinds)->with('usuario', $usuario[0])->with('errors', array())->with('results', array());
         }
     }
     public function operationUser(){
+        $session_user = $this->session->getSession();
         $input = Input::all();
-        DB::table('usuario')
+        $usuario = self::getUser($input['codigo']);
+        $kinds = self::getKind();
+        $update_paswd = 0;
+        $errors = array();
+        $results = array();
+        if($input['codigo'] == $session_user->code){
+            if(strlen($input['contrasena-ant'])>=8 && strlen($input['contrasena'])>=8){
+                if(Hash::check($input['contrasena-ant'], $usuario[0]->password)){
+                    $update_paswd = 1;
+                }else{
+                    $update_paswd = 2;
+                }
+            }
+        }
+        if($update_paswd == 0){
+            DB::table('usuario')
             ->where('codigo', $input["codigo"])
             ->update(['nombres' => $input["nombres"], 'apellidos' => $input["apellidos"], 
             'celular' => $input["celular"]]);
-        return redirect()->route('detalle-usuario', ['userid' => $input["codigo"],'view' => 'UUA']);
+            $results['user'] = 'Usuario Actualizado';
+            $usuario = self::getUser($input['codigo']);
+        }elseif($update_paswd == 1){
+            $new_password = $password = Hash::make($input['contrasena']);
+            DB::table('usuario')
+            ->where('codigo', $input["codigo"])
+            ->update(['nombres' => $input["nombres"], 'apellidos' => $input["apellidos"], 
+            'celular' => $input["celular"], 'password' => $new_password]);
+            $results['user'] = 'Usuario Actualizado';
+            $usuario = self::getUser($input['codigo']);
+        }elseif($update_paswd == 2){
+            $errors['password_user'] = 'Contraseña invalida';
+        }
+        return view('usuario-actualizar')->with('session', $this->session->getSession())
+            ->with('kinds', $kinds)->with('usuario', $usuario[0])->with('errors', $errors)->with('results', $results);
     }
     public function operationStatusUser(){
         $input = Input::all();
